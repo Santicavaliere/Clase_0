1690af733b8902dce0ec8ca4523f51bb7aa439c5
Santicavaliere <santicavaliere@gmail.com>
2024-02-05 20:11:20 -0300

    Inicio del repo

---
+++ Apuntes_de_la_primera_clase.py
@@ -0,0 +1,9 @@
+
+"""
+git init -> Inicializa un repositorio o lo reinicia (ojo con eso)
+git status -> Obtengo información del repositorio, me dice si lo es o no.
+git add [archivos] -> Agrego archivos al área de seguimiento. Es necesario para realizar commit (foto del archivo)
+
+
+
+"""
