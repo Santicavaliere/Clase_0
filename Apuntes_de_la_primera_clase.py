54c759e0ff5dc72fa9f201e3c3d9dfc4aef43c43
Santicavaliere <santicavaliere@gmail.com>
2024-02-05 21:26:22 -0300

    Segunda copia del repo

--- Apuntes_de_la_primera_clase.py
+++ Apuntes_de_la_primera_clase.py
@@ -3,7 +3,9 @@
 git init -> Inicializa un repositorio o lo reinicia (ojo con eso)
 git status -> Obtengo información del repositorio, me dice si lo es o no.
 git add [archivos] -> Agrego archivos al área de seguimiento. Es necesario para realizar commit (foto del archivo)
-
+git commit -m 'Texto explicativo de por qué realicé el commit'
+git log -> Chequea si hay commit
+git checkout  
 
 
 """
